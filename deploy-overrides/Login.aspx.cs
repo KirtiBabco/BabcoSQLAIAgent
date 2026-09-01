61373e63d1f3c842d84a4ee5bbdeac4b1db8d1bf
KirtiBabco <kirti@babcofoods.com>
2026-09-01 17:25:07 +0530

    Use Microsoft Entra login instead of temporary admin login

--- deploy-overrides/Login.aspx.cs
+++ deploy-overrides/Login.aspx.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Web;
 using System.Web.UI;
 
 namespace SQL_AI_Agent
@@ -7,30 +8,28 @@ namespace SQL_AI_Agent
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            litConfigStatus.Text = "<strong>Mode:</strong> Temporary STD testing login &nbsp; | &nbsp; <strong>Role:</strong> Admin";
+            litConfigStatus.Text = "<strong>Entra:</strong> Azure App Service Easy Auth &nbsp; | &nbsp; <strong>Admin list:</strong> " +
+                (string.IsNullOrWhiteSpace(AppConfig.AdminEmails) ? "Not set (all Entra users are standard users)" : "Configured");
 
-            if (!IsPostBack)
+            if (!IsPostBack && AuthService.BootstrapEasyAuthSession(Request))
             {
-                StartTestSessionAndContinue();
+                Response.Redirect("~/Default.aspx", false);
+                Context.ApplicationInstance.CompleteRequest();
             }
         }
 
-        protected void btnTestLogin_Click(object sender, EventArgs e)
-        {
-            StartTestSessionAndContinue();
-        }
-
-        // Kept temporarily so Login.aspx.cs can be deployed before the markup without breaking the live page.
         protected void btnEntra_Click(object sender, EventArgs e)
         {
-            StartTestSessionAndContinue();
-        }
-
-        private void StartTestSessionAndContinue()
-        {
-            AuthService.StartTemporaryAdminSession();
-            Response.Redirect("~/Default.aspx", false);
-            Context.ApplicationInstance.CompleteRequest();
+            try
+            {
+                Response.Redirect(AuthService.BuildEntraAuthorizationUrl(Request), false);
+                Context.ApplicationInstance.CompleteRequest();
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = HttpUtility.HtmlEncode(ex.Message);
+                lblMessage.Visible = true;
+            }
         }
     }
 }
